8259015adde59560f99751fe03b2adcf73f6f779
Peter Uadiale <fxavy02@gmail.com>
2025-03-17 18:47:16 +0100

    refactor: remove deprecated neonConfig declaration

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,12 +1,10 @@
-import { neon, neonConfig } from "@neondatabase/serverless"
-import { drizzle } from "drizzle-orm/neon-http"
-
-neonConfig.fetchConnectionCache = true
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 if (!process.env.DATABASE_URL) {
-    throw new Error('Database URL not found')
+  throw new Error("Database URL not found");
 }
 
-const sql = neon(process.env.DATABASE_URL)
+const sql = neon(process.env.DATABASE_URL);
 
-export const db = drizzle(sql)
+export const db = drizzle(sql);
