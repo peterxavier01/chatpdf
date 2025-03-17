5cb933d059cdd483fde4a060c657330370d80600
Peter Uadiale <fxavy02@gmail.com>
2025-03-17 18:27:06 +0100

    refactor: add file upload component, add button to manage subscription

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-import { LogIn } from "lucide-react";
+import { ArrowRight, LogIn } from "lucide-react";
 import Link from "next/link";
+import { eq } from "drizzle-orm";
+
+import FileUpload from "@/components/file-upload";
+import { Button } from "@/components/ui/button";
+import SubscriptionBtn from "@/components/subscription-btn";
+
+import { checkSubscription } from "@/lib/subscription";
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
 
 export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
+  const isPro = await checkSubscription();
+  let firstChat;
+
+  if (userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
+
+    if (firstChat) {
+      firstChat = firstChat[0];
+    }
+  }
 
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
@@ -18,7 +36,17 @@ export default async function Home() {
           </div>
 
           <div className="flex mt-4">
-            {isAuth && <Button>Go to Chats</Button>}
+            {isAuth && firstChat && (
+              <Button asChild>
+                <Link href={`/chat/${firstChat.id}`}>
+                  Go to Chats
+                  <ArrowRight className="ml-2" />
+                </Link>
+              </Button>
+            )}
+            <div className="ml-3">
+              <SubscriptionBtn isPro={isPro} />
+            </div>
           </div>
 
           <p className="max-w-xl mt-4 text-lg text-slate-600">
@@ -28,7 +56,7 @@ export default async function Home() {
 
           <div className="w-full mt-4">
             {isAuth ? (
-              <h1>File Upload</h1>
+              <FileUpload />
             ) : (
               <Link href="/sign-in" passHref legacyBehavior>
                 <Button>
