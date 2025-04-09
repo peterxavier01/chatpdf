3d1527b79c0d4cfcac63f6208ae1d38771afa15e
Peter Uadiale <fxavy02@gmail.com>
2025-04-09 01:33:24 +0100

    refactor: add logic to use local font with next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,100 @@ import Providers from "@/components/providers";
 
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const poppins = localFont({
+  src: [
+    {
+      path: "./fonts/Poppins-ExtraLight.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-ExtraLightItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Light.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-LightItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Thin.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-ThinItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-MediumItalic.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-ExtraBoldItalic.ttf",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-BlackItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
@@ -32,9 +117,7 @@ export default function RootLayout({
     <ClerkProvider afterSignOutUrl="/">
       <Providers>
         <html lang="en">
-          <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-          >
+          <body className={`${poppins.variable}  antialiased`}>
             {children}
             <Toaster />
           </body>
